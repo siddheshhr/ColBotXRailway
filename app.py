473c0b1c9086e2eafab973ecac3e7f767961d17b
siddheshhr <139670479+siddheshhr@users.noreply.github.com>
2024-10-22 14:54:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -147,4 +147,4 @@ async def root():
 
 # Main function to run the FastAPI app
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT",8000)))
